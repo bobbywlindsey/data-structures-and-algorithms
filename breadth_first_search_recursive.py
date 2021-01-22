2a2b9d6ca84b97e921376e791fea43c8e058897f
Bobby Lindsey <contact@bobbywlindsey.com>
2021-01-21 23:06:41 -0600

    Add recursive BFS implementation

---
+++ breadth_first_search_recursive.py
@@ -0,0 +1,56 @@
+from collections import deque
+
+
+class Vertex:
+    def __init__(self, name, edges):
+        self.name = name
+        self.edges = edges
+
+
+class Graph:
+    def __init__(self, vertices=[]):
+        if vertices:
+            self.graph = {vertex.name: vertex.edges for vertex in vertices}
+        else:
+            self.graph = {}
+
+
+    def add(self, Vertex):
+        self.graph[Vertex.name] = Vertex.edges
+
+
+    def get_neighbors(self, name):
+        return self.graph[name]
+
+
+    def bfs_recursive(self, discovered, queue):
+        # Base case: if nothing left in queue, return
+        if not queue:
+            return
+        # Recursive case
+        node_name = queue.popleft() # Dequeue
+        neighbors = self.get_neighbors(node_name)
+        print(f'neighbors: {neighbors}')
+        for neighbor in neighbors:
+            if neighbor not in discovered:
+                queue.append(neighbor)
+                discovered.append(neighbor)
+        self.bfs_recursive(discovered, queue)
+
+
+    def bfs(self, node_name):
+        queue = deque()
+        discovered = []
+        queue.append(node_name)
+        discovered.append(node_name)
+        self.bfs_recursive(discovered, queue)
+
+
+vertices = [Vertex('bob', ['alice', 'george']),
+            Vertex('george', [])]
+g = Graph(vertices)
+print(g.get_neighbors('bob'))
+g.add(Vertex('alice', ['bob', 'tom']))
+g.add(Vertex('tom', ['bob']))
+print(g.get_neighbors('alice'))
+g.bfs('alice')
